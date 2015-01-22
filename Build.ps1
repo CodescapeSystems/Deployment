9a02d1356a9186c784bc4b4aea2868ac68c95197
Shane Kingsley <shane.kingsley@hq.bcs.org.uk>
2015-01-22 17:04:38 +0000

    adding scripts for reference

---
+++ Build.ps1
@@ -0,0 +1,67 @@
+Framework "4.0"
+
+properties {
+    $baseDir = resolve-path .\
+    $solutionName = "Bcs.Imis.sln"
+	  $nuget = "$baseDir\packages\NuGet.CommandLine.2.8.2\tools\NuGet.exe"
+	  $nunit = "$baseDir\packages\NUnit.Runners.2.6.3\tools\nunit-console.exe"
+
+    if(!$version)
+    {
+        $version = "1.0.0.0"
+    }
+
+    Write-Host "Generating version $version"
+}
+
+task default -depends Package
+
+task Init {
+    Write-Host "Clean test results"
+	delete_file $baseDir\UnitTestResult.xml
+	delete_file $baseDir\IntegrationTestResult.xml
+}
+
+task RestorePackages -depends Init {
+    Write-Host "Restoring NuGet packages"
+    exec { & $nuget restore $solutionName }
+}
+
+task Compile -depends RestorePackages {
+    Write-Host "Build Debug"
+    Write-Host "Cleaning the solution $solutionName"
+    exec { msbuild /t:clean /v:q /nologo /p:Configuration=Debug $baseDir\$solutionName }
+    Write-Host "Building the solution"
+    exec { msbuild /t:build /v:q /nologo /p:Configuration=Debug $baseDir\$solutionName }
+
+    Write-Host "Build Release"
+    Write-Host "Cleaning the solution $solutionName"
+    exec { msbuild /t:clean /v:q /nologo /p:Configuration=Release $baseDir\$solutionName }
+    Write-Host "Building the solution"
+    exec { msbuild /t:build /v:q /nologo /p:Configuration=Release $baseDir\$solutionName }
+}
+
+task UnitTest -depends Compile {
+    exec { & $nunit  $baseDir\Bcs.Imis.UnitTests\bin\Debug\Bcs.Imis.UnitTests.dll /result=$baseDir\UnitTestResult.xml }
+}
+
+task Package -depends UnitTest {
+    exec { msbuild  $baseDir\Bcs.Imis\Bcs.Imis.csproj /t:package /p:RunOctoPack=true /p:Configuration=Release }
+    exec { msbuild  $baseDir\Bcs.ImisAdministration\Bcs.ImisAdministration.csproj /t:package /p:RunOctoPack=true /p:Configuration=Release }
+}
+
+task IntegrationTest -depends Init {
+    exec { & $nunit  $baseDir\Bcs.Imis.IntegrationTests\bin\Debug\Bcs.Imis.IntegrationTests.dll /result=$baseDir\IntegrationTestResult.xml }
+}
+
+function stop_iis_express() {
+    if (Get-Process iisexpress -ErrorAction silentlycontinue) {
+		Stop-Process -processname iisexpress
+	}
+}
+
+function global:delete_file($file) {
+    if($file) { remove-item $file -force -ErrorAction SilentlyContinue | out-null } 
+}
+
+ 
